073c0e54d12a8d1118bd47f747be2e237371fcdd
Muhammad Fouzan Abid <abidmuhammadfouzan@gmail.com>
2026-01-02 12:43:10 +0500

    Update title and enable dataframe display

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from snowflake.snowpark.functions import col
 import pandas as pd
 
 # Write directly to the app
-st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
+st.title(f":cup_with_straw: Customize Your Smoothie Never!:cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """)
@@ -20,7 +20,7 @@ session = cnx.session()
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
 # Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
